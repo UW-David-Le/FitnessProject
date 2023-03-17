5fe247f1ad137015bc86f92d66b2edd8503fd118
Andrew Amaral <andrewamaral@Andrews-Air.localdomain>
2023-03-17 14:46:14 -0400

    second last push

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import "./Footer.scss";
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__cardContainer">
+        <div className="footer__card">
+        Gorilla Gang
+        </div>
+        <div className="footer__card">
+        Koko the Gorilla</div>
+        <div className="footer__card">
+        Chimp Champs
+        </div>
+      </div>
+    </div>
+  );
+}
