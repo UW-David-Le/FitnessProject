be7c7e457e6170ca21b8b7c127f7d67283f6348b
DAVID LE <david8693@live.com>
2023-04-11 23:42:47 -0400

    bug fixes

--- src/pages/PersonalWorkout/PersonalWorkout.js
+++ src/pages/PersonalWorkout/PersonalWorkout.js
@@ -3,9 +3,16 @@ import MuscleCard from "../../components/MuscleCard/MuscleCard"
 
 export default function PersonalWorkout () {
     const [currentWorkout, setCurrentWorkout] = useState([])
-
+    
+    const personal = () => {
+        setCurrentWorkout()
+    }
+    
     
     return (
+        <>
         <div> {currentWorkout}</div>
+        
+      </>
     )   
 }
